019f90e5d3697e707a006cf29fdc853ddaff477c
Yash-Azure <64698286+Yash-Azure@users.noreply.github.com>
2022-10-03 17:44:40 +0530

    Update app-lb.tf

--- 3-tier-Challenge/app-lb.tf
+++ 3-tier-Challenge/app-lb.tf
@@ -6,44 +6,42 @@ resource "azurerm_lb" "ylb" {
 
   frontend_ip_configuration {
     name                          = "app-fe"
-   # public_ip_address_id          = azurerm_public_ip.ypip.id
     subnet_id                     = azurerm_subnet.ysubnet[0].id
-    private_ip_address_allocation = Dynamic
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_lb_backend_address_pool" "ybackend" {
-  name                = "app-pool"
-  loadbalancer_id     = azurerm_lb.ylb.id
+  name            = "app-pool"
+  loadbalancer_id = azurerm_lb.ylb.id
 }
 
 resource "azurerm_lb_probe" "yprobe" {
-    name                = "https-probe"
-    resource_group_name = azurerm_resource_group.yrg.name
-    loadbalancer_id     = azurerm_lb.ylb.id
-    protocol            = "TCP"
-    port                = "5072"
-  }
+  name            = "https-probe"
+  loadbalancer_id = azurerm_lb.ylb.id
+  protocol        = "Tcp"
+  port            = "5072"
+}
 
 resource "azurerm_lb_rule" "yrule" {
-    name                           = "app-rule"
-    resource_group_name = azurerm_resource_group.yrg.name
-    loadbalancer_id                = azurerm_lb.ylb.id
-    protocol                       = "TCP"
-    frontend_port                  = "5072"
-    backend_port                   = "5072"
-    frontend_ip_configuration_name = "myapp"
-    enable_floating_ip             = false
-    backend_address_pool_id        = azurerm_lb_backend_address_pool.ybackend.id
-    idle_timeout_in_minutes        = 5
-    probe_id                       = element(azurerm_lb_probe.azlb.*.id, count.index)
-  }
+  count                          = 1
+  name                           = "app-rule"
+  loadbalancer_id                = azurerm_lb.ylb.id
+  protocol                       = "Tcp"
+  frontend_port                  = "5072"
+  backend_port                   = "5072"
+  frontend_ip_configuration_name = "app-fe"
+  enable_floating_ip             = false
+  idle_timeout_in_minutes        = 5
+  probe_id                       = element(azurerm_lb_probe.yprobe.*.id, count.index)
+}
 
-  resource "azurerm_network_interface_backend_address_pool_association" "yaso" {
-network_interface_id = azurerm_network_interface.vm[count.index].id
-ip_configuration_name = app-vm-${count.index + 1}-config
-bbackend_address_pool_id = azurerm_lb_backend_address_pool.ybackend.id
-depends_on = [
-  azurerm_lb_backend_address_pool
-]
-  }
+resource "azurerm_network_interface_backend_address_pool_association" "yaso" {
+  count                   = 1
+  network_interface_id    = azurerm_network_interface.vm[count.index].id
+  ip_configuration_name   = "web-vm-ip-${count.index + 1}"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.ybackend.id
+  depends_on = [
+    azurerm_lb_backend_address_pool.ybackend
+  ]
+}
