121d69fc355ddd847bedcd5661994871a0f0789d
Yash-Azure <64698286+Yash-Azure@users.noreply.github.com>
2022-10-05 13:35:03 +0530

    Update metadata-challenge.ps1

--- metadata-challenge/metadata-challenge.ps1
+++ metadata-challenge/metadata-challenge.ps1
@@ -1,11 +1,22 @@
 # REST call provide us the output in Object format, no need to apply a conversion.
 
-$dataByREST=Invoke-RestMethod -Headers @{"Metadata"="true"} -Uri "http://169.254.169.254/metadata/instance?api-version=2021-01-01"
-$dataByREST.compute
-$dataByREST.compute.location
+$dataByREST_v1=Invoke-RestMethod -Headers @{"Metadata"="true"} -Uri "http://169.254.169.254/metadata/instance?api-version=2021-12-13"
+$dataByREST_v1.compute
+$dataByREST_v1.compute.location
+
+$dataByREST_v2=Invoke-RestMethod -Headers @{"Metadata"="true"} -Uri "http://169.254.169.254/metadata/instance/compute/?api-version=2021-12-13"
+$dataByREST_v2
+$dataByREST_v2.location
+
+$dataByREST_v3=Invoke-RestMethod -Headers @{"Metadata"="true"} -Uri "http://169.254.169.254/metadata/instance/compute/location?api-version=2021-12-13&format=text"
+$dataByREST_v3
+
+$dataByREST_v4=Invoke-RestMethod -Headers @{"Metadata"="true"} -Uri "http://169.254.169.254/metadata/instance/network/interface/0?api-version=2021-12-13"
+$dataByREST_v4.ipv4.ipAddress
+
 
 # WEB call respondes with JSON format. To parse the data, we can use convertfrom-json cmdlet to parse the values.
-$dataByweb = Invoke-WebRequest -Headers @{"Metadata"="true"} -Uri "http://169.254.169.254/metadata/instance?api-version=2021-01-01"
+$dataByweb = Invoke-WebRequest -Headers @{"Metadata"="true"} -Uri "http://169.254.169.254/metadata/instance?api-version=2021-12-13"
 $dataByweb.Content
 $formated_web_data = $dataByweb.Content | ConvertFrom-Json
 $formated_web_data
